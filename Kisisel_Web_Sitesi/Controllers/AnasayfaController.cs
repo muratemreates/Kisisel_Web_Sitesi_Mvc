8b83ce815296a028d43df9fe4a9bc7c4330167c9
Murat Emre <atess.muratemre@gmail.com>
2024-04-30 22:09:23 +0300

    Add project files.

---
+++ Kisisel_Web_Sitesi/Controllers/AnasayfaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kisisel_Web_Sitesi.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        // GET: Anasayfa
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimda()
+        {
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Works()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kisisel_Web_Sitesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kisisel_Web_Sitesi.MvcApplication" Language="C#" %>
